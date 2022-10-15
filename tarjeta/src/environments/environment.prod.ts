5dc8e0186f0ed67cb1a40c10c647d766dfbdf516
Monroy2021 <luismonroy1014@gmail.com>
2022-10-15 09:22:49 -0500

    configuracion de firebase

--- tarjeta/src/environments/environment.prod.ts
+++ tarjeta/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBPsNPIEmXj_O6TlEm9y7Q4PqekXgR2LuU",
+    authDomain: "tarjetacredito-79fbf.firebaseapp.com",
+    projectId: "tarjetacredito-79fbf",
+    storageBucket: "tarjetacredito-79fbf.appspot.com",
+    messagingSenderId: "1080079757967",
+    appId: "1:1080079757967:web:7aaf32ef29c310ad4ef6fa"
+  }
 };
--- tarjeta/src/environments/environment.ts
+++ tarjeta/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false, 
+  firebase: {
+   apiKey: "AIzaSyBPsNPIEmXj_O6TlEm9y7Q4PqekXgR2LuU",
+  authDomain: "tarjetacredito-79fbf.firebaseapp.com",
+  projectId: "tarjetacredito-79fbf",
+  storageBucket: "tarjetacredito-79fbf.appspot.com",
+  messagingSenderId: "1080079757967",
+  appId: "1:1080079757967:web:7aaf32ef29c310ad4ef6fa"
+  }
 };
 
 /*
